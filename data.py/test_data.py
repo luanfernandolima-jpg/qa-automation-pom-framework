0aba7000b50f0c69139c1433a91cc24c52274a2d
Luan Fernando de Lima <luan.fernando_lima@hotmail.com>
2026-05-29 17:05:39 -0300

    refactor: clean POMframework structure

---
+++ data.py/test_data.py
@@ -0,0 +1,8 @@
+URBAN_ROUTES_URL = 'https://cnt-ee756e96-51ff-43db-ad87-401aa458b886.containerhub.tripleten-services.com/?lng=pt'
+
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
---
+++ tests/test_main.py
@@ -0,0 +1,114 @@
+from utils import helpers
+import data
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from pages.pages import UrbanRoutesPage
+
+
+class TestUrbanRoutes:
+
+    @classmethod
+    def setup_class(cls):
+        # Configura as opções do Chrome com logs de performance habilitados
+        # Necessário para que retrieve_phone_code consiga capturar o código via Network logs
+        chrome_options = Options()
+        chrome_options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
+
+        cls.driver = webdriver.Chrome(options=chrome_options)
+        cls.driver.implicitly_wait(5)
+
+        # Verifica se o servidor Urban Routes está acessível antes de iniciar os testes
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print("Conectado ao servidor Urban Routes")
+        else:
+            print("Não é possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execução")
+
+    def test_set_route(self):
+        self.driver.get(data.URBAN_ROUTES_URL)
+        routes_page = UrbanRoutesPage(self.driver)
+
+        routes_page.set_route_addresses(data.ADDRESS_FROM, data.ADDRESS_TO)
+
+        assert routes_page.get_from_field_text() == data.ADDRESS_FROM
+        assert routes_page.get_to_field_text() == data.ADDRESS_TO
+
+    def test_select_plan(self):
+        self.driver.get(data.URBAN_ROUTES_URL)
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.set_route_addresses(data.ADDRESS_FROM, data.ADDRESS_TO)
+        routes_page.click_call_a_taxi_button()
+
+        routes_page.click_comfort_option()
+
+        assert routes_page.get_selected_tariff_option() == "Comfort"
+
+    def test_fill_phone_number(self):
+        self.driver.get(data.URBAN_ROUTES_URL)
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.set_route_addresses(data.ADDRESS_FROM, data.ADDRESS_TO)
+        routes_page.click_call_a_taxi_button()
+
+        routes_page.set_phone(data.PHONE_NUMBER)
+
+        assert routes_page.get_inserted_phone_number() == data.PHONE_NUMBER
+
+    def test_fill_card(self):
+        self.driver.get(data.URBAN_ROUTES_URL)
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.set_route_addresses(data.ADDRESS_FROM, data.ADDRESS_TO)
+        routes_page.click_call_a_taxi_button()
+
+        routes_page.set_card(data.CARD_NUMBER, data.CARD_CODE)
+
+        assert routes_page.get_current_payment_method() == 'Cartão'
+
+    def test_comment_for_driver(self):
+        self.driver.get(data.URBAN_ROUTES_URL)
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.set_route_addresses(data.ADDRESS_FROM, data.ADDRESS_TO)
+        routes_page.click_call_a_taxi_button()
+
+        routes_page.set_message_for_driver(data.MESSAGE_FOR_DRIVER)
+
+        assert routes_page.get_message_for_driver() == data.MESSAGE_FOR_DRIVER
+
+    def test_order_blanket_and_handkerchiefs(self):
+        self.driver.get(data.URBAN_ROUTES_URL)
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.set_route_addresses(data.ADDRESS_FROM, data.ADDRESS_TO)
+        routes_page.click_call_a_taxi_button()
+        routes_page.click_comfort_option()
+
+        routes_page.click_blanket_and_handkerchiefs_option()
+
+        assert routes_page.is_blanket_and_handkerchiefs_option_checked()
+
+    def test_order_2_ice_creams(self):
+        self.driver.get(data.URBAN_ROUTES_URL)
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.set_route_addresses(data.ADDRESS_FROM, data.ADDRESS_TO)
+        routes_page.click_call_a_taxi_button()
+        routes_page.click_comfort_option()
+
+        desired_icecream_amount = 2
+        routes_page.add_icecream(desired_icecream_amount)
+
+        assert routes_page.get_current_icecream_amount() == desired_icecream_amount
+
+    def test_car_search_model_appears(self):
+        self.driver.get(data.URBAN_ROUTES_URL)
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.set_route_addresses(data.ADDRESS_FROM, data.ADDRESS_TO)
+        routes_page.click_call_a_taxi_button()
+        routes_page.click_comfort_option()
+        routes_page.set_message_for_driver(data.MESSAGE_FOR_DRIVER)
+
+        routes_page.click_order_button()
+
+        assert routes_page.is_order_taxi_popup_displayed()
+
+    @classmethod
+    def teardown_class(cls):
+        # Fecha o navegador após a execução de todos os testes
+        cls.driver.quit()
